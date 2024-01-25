097b5f409b4aac12249f6b53bafc3b35dde2fee6
Igor Chorazewicz <igor.chorazewicz@intel.com>
2024-01-25 22:17:40 +0100

    XXX

--- test/test_mmap.c
+++ test/test_mmap.c
@@ -1,22 +1,23 @@
+#include <numa.h>
+#include <numaif.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/mman.h>
 #include <sys/syscall.h>
 #include <unistd.h>
-#include <numa.h>
-#include <numaif.h>
-#include <stdlib.h>
 
 int main() {
-    void *ptr = mmap(NULL, 4096, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS,
-                -1, 0);
+    void *ptr = mmap(NULL, 4096, PROT_READ | PROT_WRITE,
+                     MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
     if (!ptr) {
         fprintf(stderr, "mmap failed\n");
         perror("mmap");
         abort();
     }
 
-    struct bitmask* allowed_nodes = numa_get_mems_allowed();
-    int ret = mbind(ptr, 4096, MPOL_BIND, allowed_nodes->maskp, allowed_nodes->size, 0);
+    struct bitmask *allowed_nodes = numa_get_mems_allowed();
+    int ret = mbind(ptr, 4096, MPOL_BIND, allowed_nodes->maskp,
+                    allowed_nodes->size, 0);
     if (ret) {
         perror("mbind");
         abort();
