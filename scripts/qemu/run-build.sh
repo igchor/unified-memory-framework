df8b6c1dbed923594759b0b34d5aa18aec724d35
Igor Chorążewicz <igor.chorazewicz@intel.com>
2024-03-05 12:21:29 -0800

    Add simple qemu run to CI

---
+++ scripts/qemu/run-build.sh
@@ -0,0 +1,21 @@
+repo=$1
+branch=$2
+
+git clone $repo umf
+cd umf
+git checkout $branch
+
+cmake -B build
+    -DCMAKE_BUILD_TYPE=Debug
+    -DUMF_BUILD_OS_MEMORY_PROVIDER=ON
+    -DUMF_ENABLE_POOL_TRACKING=ON
+    -DUMF_FORMAT_CODE_STYLE=OFF
+    -DUMF_DEVELOPER_MODE=ON
+    -DUMF_BUILD_LIBUMF_POOL_DISJOINT=ON
+    -DUMF_BUILD_EXAMPLES=ON
+    -DUMF_BUILD_LEVEL_ZERO_PROVIDER=OFF
+
+cmake --build build -j $(nproc)
+
+cd build
+ctest --output-on-failure --test-dir test
