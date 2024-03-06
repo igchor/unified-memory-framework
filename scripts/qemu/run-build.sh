42bcd78c48ecac9d9e58911f37dc8bb97cc48186
Igor Chorążewicz <igor.chorazewicz@intel.com>
2024-03-05 18:07:57 -0800

    XXX

--- scripts/qemu/run-build.sh
+++ scripts/qemu/run-build.sh
@@ -1,7 +1,7 @@
 repo=$1
 branch=$2
 
-sudo apt-get update && sudo apt-get install -y git libnuma-dev libjemalloc-dev libtbb-dev libhwloc-dev cmake gcc
+sudo dpkg -i /home/cxltest/*.deb
 
 git clone $repo umf
 cd umf
